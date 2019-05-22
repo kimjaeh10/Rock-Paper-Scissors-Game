952adfda8b56f12a698a15ece4d79faec78ce6a9
Jaehyung Kim <kimjaeh10@gmail.com>
2019-05-22 17:40:51 -0400

    Added more features

--- js/app.js
+++ js/app.js
@@ -7,11 +7,37 @@ const result_p = document.querySelector(".result > p");
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
+const computer_questionmark = document.getElementById("question-mark");
 
 function getCompChoice() {
     const choices = ['r', 'p', 's'];
     const randomNumber = Math.floor(Math.random() * 3);
-    return choices[randomNumber];
+    computer_questionmark.innerHTML = "";
+    const choice = choices[randomNumber];
+    showComputerChoice(choice);
+    
+    return choice;
+}
+
+function showComputerChoice(letter) {
+    if (letter === 'r') {
+        var img = document.createElement("IMG");
+        img.src = "images/rock.png";
+        img.width = 100;
+        computer_questionmark.appendChild(img);
+    }
+    if (letter === 'p') {
+        var img = document.createElement("IMG");
+        img.src = "images/paper.png";
+        img.width = 100;
+        computer_questionmark.appendChild(img);
+    }
+    if (letter === 's') {
+        var img = document.createElement("IMG");
+        img.src = "images/scissor.png";
+        img.width = 100;
+        computer_questionmark.appendChild(img);
+    }
 }
 
 function game(userChoice) {
@@ -44,14 +70,13 @@ function convertToWord(letter) {
     return "Scissors";
 }
 
-// setTimeout(function () { console.log("3 seconds") }, 3000 );
 
 function win(user, comp) {
     userScore++;
     userScore_span.innerHTML = userScore;
     result_p.innerHTML = `${convertToWord(user)} beats ${convertToWord(comp)}. You Win!`;
     document.getElementById(user).classList.add('green-glow');
-    setTimeout(function () { document.getElementById(user).classList.remove('green-glow') }, 500);
+    setTimeout(function () { document.getElementById(user).classList.remove('green-glow') }, 300);
 }
 
 function lose(user, comp) {
@@ -59,13 +84,13 @@ function lose(user, comp) {
     compScore_span.innerHTML = compScore;
     result_p.innerHTML = `${convertToWord(comp)} beats ${convertToWord(user)}. You Lost.`;
     document.getElementById(user).classList.add('red-glow');
-    setTimeout(function () { document.getElementById(user).classList.remove('red-glow') }, 500);
+    setTimeout(function () { document.getElementById(user).classList.remove('red-glow') }, 300);
 }
 
 function draw(user, comp) {
     result_p.innerHTML = `${convertToWord(comp)} equals ${convertToWord(user)}. It's a draw.`
     document.getElementById(user).classList.add('gray-glow');
-    setTimeout(function () { document.getElementById(user).classList.remove('gray-glow') }, 500);
+    setTimeout(function () { document.getElementById(user).classList.remove('gray-glow') }, 300);
 }
 
 function main() {
@@ -80,6 +105,22 @@ function main() {
     scissors_div.addEventListener('click', function () {
         game("s");
     })
+}
+
+function myFunction() {
+    userScore = 0;
+    compScore = 0;
+    userScore_span.innerHTML = userScore;
+    compScore_span.innerHTML = compScore;
+    result_p.innerHTML = "Rock Paper Scissors Game";
+
+    computer_questionmark.innerHTML = "";
+
+    var img = document.createElement("IMG");
+    img.src = "images/questionmark.png";
+    img.alt = "";
+    img.width = 100;
+    computer_questionmark.appendChild(img);
 }
 
 main();
